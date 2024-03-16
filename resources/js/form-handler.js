118bff05a6ddf97aacb7f43030b724db77c35978
gpaphitis <giorgospaphitis@gmail.com>
2024-03-16 19:11:30 +0200

    Improved form handler
    Instead of being dependent on input and empty error being the same count, now each input that is being checked must be wrapped in an element with form-field class which will also contain its respective empty error message

--- resources/js/form-handler.js
+++ resources/js/form-handler.js
@@ -1,39 +1,100 @@
 (function (global)
 {
    let formHandler = {};
+   /**
+    * @abstract Clears form fields and hides empty errors.
+    * 
+    * Only inputs and wrapped in an element with class name "form-field" are cleared
+    */
    function clearForm()
    {
-      // Clears all empty field errors
-      let inputs = document.querySelectorAll("input");
-      let emptyErrors = document.querySelectorAll(".empty-error");
-      for (let i = 0; i < emptyErrors.length; i++) {
-         inputs[i].value = "";
-         makeInvisible(emptyErrors[i]);
+      let inputs = document.querySelectorAll(".form-field");
+      for (let i = 0; i < inputs.length; i++) {
+         for (let j = 0; j < inputs[i].children.length; j++) {
+            if (inputs[i].children[j].tagName === "INPUT") {
+               inputs[i].children[j].value = "";
+               hideEmptyError(inputs[i]);
+            }
+         }
       }
    }
+   /**
+    * @abstract Checks if fields are empty and shows or hides their respective empty errors
+    * 
+    * Only inputs and wrapped in an element with class name "form-field" are checked
+    */
    function isFieldEmpty()
    {
-      let inputs = document.querySelectorAll("input");
-      let emptyErrors = document.querySelectorAll(".empty-error");
+      let inputs = document.querySelectorAll(".form-field");
       let isEmpty = false;
       for (let i = 0; i < inputs.length; i++) {
-         if (inputs[i].value === "") {
-            makeVisible(emptyErrors[i]);
-            isEmpty = true;
+         for (let j = 0; j < inputs[i].children.length; j++) {
+            if (inputs[i].children[j].tagName === "INPUT") {
+               if (inputs[i].children[j].value === "") {
+                  isEmpty = true;
+                  showEmptyError(inputs[i]);
+                  break;
+               }
+               hideEmptyError(inputs[i]);
+               break;
+            }
          }
-         else
-            makeInvisible(emptyErrors[i]);
       }
       return isEmpty;
    }
+   /**
+    *@abstract Finds child with "empty-error" class and makes it visible
+
+    * Makes error visible by removing "hidden" class
+    * 
+    * @param {HTMLElement} parent Parent element with the error as child
+    */
+   function showEmptyError(parent)
+   {
+      for (let i = 0; i < parent.children.length; i++) {
+         if (parent.children[i].classList.contains("empty-error")) {
+            parent.children[i].classList.remove("hidden");
+            return;
+         }
+      }
+   }
+   /**
+    * @abstract Finds child with empty-error class and makes it invisible
+    * 
+    * Makes error invisible by adding "hidden" class
+    * 
+    * @param {HTMLElement} parent Parent element with the error as child
+    */
+   function hideEmptyError(parent)
+   {
+      for (let i = 0; i < parent.children.length; i++) {
+         if (parent.children[i].classList.contains("empty-error"))
+            parent.children[i].classList.add("hidden");
+      }
+   }
+   /**
+    *@abstract  Makes element visible
+    *
+    * @param {HTMLElement} element Element to be made visible
+    */
    function makeVisible(element)
    {
       element.classList.remove("hidden");
    }
+   /**
+    *@abstract  Makes element invisible
+    *
+    * @param {HTMLElement} element Element to be made invisible
+    */
    function makeInvisible(element)
    {
       element.classList.add("hidden");
    }
+   /**
+    * @abstract Checks if email is valid and shows or hides error message
+    * 
+    * Email input must have id "target-email" and email error id ""
+    */
    function isEmailValid()
    {
       if (document.querySelector("#target-email:invalid") != null) {
